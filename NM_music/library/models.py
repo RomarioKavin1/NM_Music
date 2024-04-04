d901723abeedb0287d29c0a1154fe6082b1f4a57
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-04-04 23:58:56 +0530

    models

--- NM_music/library/models.py
+++ NM_music/library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Album(models.Model):
+    artist = models.ForeignKey(Artist, related_name='albums', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return f'{self.title} by {self.artist.name}'
+
+class Track(models.Model):
+    album = models.ForeignKey(Album, related_name='tracks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    duration = models.IntegerField(help_text="Duration in seconds")
+
+    def __str__(self):
+        return self.title
