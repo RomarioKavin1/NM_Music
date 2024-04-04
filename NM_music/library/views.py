dcb9e2bf388b856391a9ad693d8d708fa49d6289
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-04-04 23:59:50 +0530

    views

--- NM_music/library/views.py
+++ NM_music/library/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Artist
 
-# Create your views here.
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'library/artist_list.html', {'artists': artists})
