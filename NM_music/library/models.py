e1560fb6995f2bdfbb38790e66221d78f465eadb
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-04-08 00:33:38 +0530

    models

--- NM_music/library/models.py
+++ NM_music/library/models.py
@@ -4,6 +4,7 @@ class Artist(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
     bio = models.TextField(blank=True)  
+    image = models.URLField(blank=True)
 
     def __str__(self):
         return self.name
@@ -12,7 +13,7 @@ class Album(models.Model):
     artist = models.ForeignKey(Artist, related_name='albums', on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     release_date = models.DateField()
-    image = models.ImageField(upload_to='album_covers/', blank=True) 
+    image = models.URLField(blank=True)
 
     def __str__(self):
         return f'{self.title} by {self.artist.name}'
