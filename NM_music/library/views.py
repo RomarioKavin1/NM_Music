d2a8bd67e9bddc7435ff1655ce491bbe9931de0c
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-04-08 00:02:39 +0530

    ui

--- NM_music/library/views.py
+++ NM_music/library/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
+
 from .models import Artist
 
 def artist_list(request):
